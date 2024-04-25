aafdf5806fb3a65fb8037bb0544d1ed4d268de7d
BilalMO <azbilal.motassim@gmail.com>
2024-04-25 19:16:35 +0100

    init

---
+++ src/pages/Login.jsx
@@ -0,0 +1,26 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Unstable_Grid2';
+
+const Login = () => {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2} sx={{marginTop:20}}>
+                <Grid xs={2}>
+                    
+                </Grid>
+                <Grid xs={10}>
+                                    // HNA INPUTS!!!!!!!!!!!!!!!!!!!! DIR BOX
+                </Grid>
+                <Grid xs={2}>
+                    
+                </Grid>
+                <Grid xs={4}>
+                    /////  BUTTON
+                </Grid>
+            </Grid>
+        </Box>
+    );
+}
+
+export default Login;
